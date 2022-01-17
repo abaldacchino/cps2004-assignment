57ed162264a9a0a9aeadef3de14a57417e9af9dd
Adriana Baldacchino <baldacchino.adriana@gmail.com>
2022-01-17 16:20:56 +0100

    Fixed bug with left shift, string constructor is working now

--- task3/bigint.hpp
+++ task3/bigint.hpp
@@ -13,8 +13,6 @@
 #endif
 
 typedef uint64_t word_t;
-//maximum 11 words hence no of words can be stored in 8 bits
-typedef uint_least8_t word_count_t;  
 
 template <int bits>
 class myuint{
@@ -24,7 +22,6 @@ class myuint{
     static_assert(bits>=1 && bits<=2048, "Range of bit number is from 1 to 2048");
 
     private:
-        const word_count_t max_words = bits>>6;
         // stores individual words (of size 64 bits) of the integer
         // with least significant word first
         std::array<word_t, (bits>>6)> words{};
@@ -46,7 +43,7 @@ class myuint{
         }
 
         //Unsigned Integer constructor
-        myuint(uint num){
+        myuint(uint64_t num){
             //num should fit in 64 bits since it is of type uint
             words[0] = num;
         }
@@ -58,9 +55,9 @@ class myuint{
         myuint(std::string s){    
             myuint<bits> ten(10);           //stores value 10 in myuint datatype
             myuint<bits> ten_power_i(1);
-            for(int i=0; i<s.size(); i++){
+            for(int i=s.size()-1; i>=0; i--){
                 myuint<bits> buffer(s[i] - '0');   //gets ith digit of string
-                this += buffer * ten_power_i;
+                *this += buffer * ten_power_i;
                 ten_power_i *=ten;
             }
         }
@@ -72,18 +69,17 @@ class myuint{
         myuint, in little endian format (least significant digit first)
         -- Ignores any excess digits 
         
+        
         myuint(std::string s){    
             char *token = strtok(&(s[0]), " ");
-            for(word_count_t i=0; i< max_words; i++){
+            for(int i=0; i< words.size(); i++){
                 if(token != NULL){
                     //stoull throws invalid_argument exception in case of token not being an unsigned integer
                     words[i] = std::stoull(token);
                     token = strtok(NULL, " ");
                 }
             }
-        }
-        */
-
+        }*/
 
         // Constructor that accepts array of (base 64) digits
         myuint(std::array<word_t, (bits>>6)> input){    
@@ -97,18 +93,7 @@ class myuint{
                 return *this;
             }
             //Copy array (std::array has already defined copy operator)
-            other.words = words;
-            return *this;
-        }
-
-        // Move assignment
-        myuint<bits>& operator=(myuint<bits>&& other) noexcept{
-            // Guard self assignment
-            if (this == &other){
-                return *this;
-            }
-
-            words = std::exchange(other.words, nullptr); // leave other in valid state
+            words = other.words;
             return *this;
         }
 
@@ -117,7 +102,7 @@ class myuint{
         myuint<bits>& operator++(){
             words[0]++; //Adds 1 to least significant digit
             //Accounts for carry (case of overflow)
-            for(word_count_t i=1; i<max_words; i++){
+            for(int i=1; i<words.size(); i++){
                 if(words[i-1]==0){
                     words[i]++;
                 }else{
@@ -138,7 +123,7 @@ class myuint{
         myuint<bits>& operator--(){
             words[0]--; //Removes 1 to least significant digit
             //Accounts for carry (case of underflow)
-            for(word_count_t i=1; i<max_words; i++){
+            for(int i=1; i<words.size(); i++){
                 if(words[i-1]==UINT64_MAX){
                     words[i]--;
                 }else{
@@ -161,7 +146,7 @@ class myuint{
             //carry ==1 if words[0] overflows (wraps around), 0 otherwise
             //this way conditionals are avoided
             int carry = words[0] < rhs.words[0];    
-            for(word_count_t i=1; i<max_words; i++){
+            for(int i=1; i<words.size(); i++){
                 words[i] += (rhs.words[i] + carry);
                 carry = words[i] < (rhs.words[i] + carry);
             }
@@ -179,8 +164,8 @@ class myuint{
             words[0] -= rhs.words[0];
             //carry ==1 if words[0] underflows (wraps around), 0 otherwise
             //this way conditionals are avoided
-            int carry = words[0] > rhs.words[0];    
-            for(word_count_t i=1; i<max_words; i++){
+            int carry = words[0] > words[0] + rhs.words[0];    
+            for(int i=1; i<words.size(); i++){
                 words[i] -= (rhs.words[i] + carry);
                 carry = words[i] > (words[i] + rhs.words[i] + carry);
             }
@@ -201,18 +186,18 @@ class myuint{
 
             if(rem==0){
                 int i=0;
-                while(i<max_words-word_shift){
+                while(i<words.size()-word_shift){
                     words[i] = words[i+word_shift];
                     i++;
                 }
-                while(i<max_words){
+                while(i<words.size()){
                     words[i]=0;
                     i++;
                 }
             }else{
                 int i=0;
                 //Case where words[i] is entirely filled
-                while(i<max_words-(word_shift+1)){
+                while(i<words.size()-(word_shift+1)){
                     /* Performing shift by getting highest bits from words[i+word_shift]
                        and lowest bits from words[i+word_shift+1]
                     */
@@ -223,9 +208,10 @@ class myuint{
                 //Case where words[i] only partially filled
                 //Only lowest bits of words[i] filled
                 words[i] =  get_hi(words[i+word_shift], 64-rem);
+                i++;
 
                 //Case where words[i] not filled (0)
-                while(i<max_words){
+                while(i<words.size()){
                     words[i]=0;
                     i++;
                 }
@@ -246,7 +232,7 @@ class myuint{
             uint rem = rhs - (word_shift<<6);    //Finding remainder
 
             if(rem==0){
-                int i=max_words-1;
+                int i=words.size()-1;
                 while(i>=word_shift){
                     words[i] = words[i-word_shift];
                     i--;
@@ -257,18 +243,19 @@ class myuint{
                 }
             }else{
                 //Case where words[i] entirely filled
-                int i=max_words-1;
+                int i=words.size()-1;
                 while(i>=word_shift+1){
                     /* Performing shift by getting lowest bits from words[i-word_shift]
                        and highest bits from words[i-(word_shift)-1]
                     */
-                    words[i] =  (get_lo(words[i-word_shift], 64-rem)<<(64-rem))
+                    words[i] =  (get_lo(words[i-word_shift], 64-rem)<<rem)
                         | (get_hi(words[i-word_shift-1], rem));
                     i--;
                 }
                 //Case where words[i] is only partially filled
                 //Only highest bits of words[i] filled
-                words[i] =  (get_lo(words[i-word_shift], 64-rem)<<(64-rem));
+                words[i] =  (get_lo(words[i-word_shift], 64-rem)<<rem);
+                i--;
 
                 //Case where words[i] is not filled (0)
                 while(i>=0){
@@ -295,13 +282,13 @@ class myuint{
                order to calculate overflow. In a multiplication heavy application
                of bigint, ideally words should be stored in 32 bits immediately
             */
-            for(word_count_t i=0; i<max_words; i++){
-                for(word_count_t j=0; j<max_words; j++){
+            for(int i=0; i<words.size(); i++){
+                for(int j=0; j<words.size(); j++){
                     //Resulting multiplication is multiplied by (64)^index
                     uint index= i+j;
 
                     //Any overflow is ignored (making calculation mod bits)
-                    if(index>=max_words)continue;
+                    if(index>=words.size())continue;
 
                     // Calculating coefficients a1b1(k)^2 + (a1b0+a0b1)k + a0b0
                     // where a1 = highest 32 bits of a, a0 lowest 32 bits of a
@@ -309,18 +296,17 @@ class myuint{
                     uint64_t s1 = get_lo(words[i], 32)* get_hi(rhs.words[j], 32);
                     uint64_t s2 = get_hi(words[i], 32)* get_lo(rhs.words[j], 32);
 
+
                     s1 += s2;
 
                     myuint<bits> temp;
                     temp.words[index] = s0 + (get_lo(s1, 32) <<32);
-                    if(index+1<max_words){
+                    if(index+1<words.size()){
                         uint64_t s3 = get_hi(words[i], 32)* get_hi(rhs.words[j], 32);
                         //Case of overflow of s1+s2
                         s3 += s1<s2;
-
-                        temp.words[index+1] = s1 + get_hi(s2, 32); 
+                        temp.words[index+1] = s3 + get_hi(s1, 32); 
                     }
-
                     result+=temp;
                 }
             }
@@ -341,11 +327,12 @@ class myuint{
             myuint<bits> count;
             if(rhs==0) throw std::overflow_error("Divide by zero exception");
 
-            while(this >= rhs){
+            while(*this >= rhs){
                 count++;
-                this -= rhs;
+                *this -= rhs;
+                //std::cout << *this << std::endl;
             }
-            this =count;
+            *this =count;
             return *this;
         }
 
@@ -361,9 +348,10 @@ class myuint{
             myuint<bits> count;
             if(rhs==0) throw std::overflow_error("Divide by zero exception");
 
-            while(this >= rhs){
+            while(*this >= rhs){
                 count++;
-                this -= rhs;
+                *this -= rhs;
+                std::cout << *this << std::endl;
             }
 
             return *this;
@@ -377,7 +365,7 @@ class myuint{
 
         //Comparison operators
         inline friend bool operator< (const myuint<bits>& lhs, const myuint<bits>& rhs){ 
-            for(word_count_t i= lhs.max_words-1; i>=0; i--){
+            for(int i= lhs.words.size()-1; i>=0; i--){
                 if(lhs.words[i] < rhs.words[i])return true;
                 if(rhs.words[i] < lhs.words[i])return false;
             }
@@ -402,27 +390,28 @@ class myuint{
 
         //Write to stream (convert to base 10)
         friend std::ostream& operator<<(std::ostream& os, const myuint<bits>& bigint){
-            //Outputs representation in base 10
-            myuint<bits> ten(10);           //stores value 10 in myuint datatype
-            myuint<bits> digit;             //stores nth digit
-            myuint<bits> result;
+            // //Outputs representation in base 10
+            // myuint<bits> ten(10);           //stores value 10 in myuint datatype
+            // myuint<bits> digit;             //stores nth digit
+            // myuint<bits> result = bigint;
 
-            do{
-                digit = result % ten;   //result mod 10 gets the last digit of result
-                os << digit.words[0];   //outputs digit (stored in words[0] since value<10)
-                result /= ten;  //divides result by 10 (effectively right shifting in base 10)
-            }while(!result.is_zero());
+            // do{
+            //     digit = result % ten;   //result mod 10 gets the last digit of result
+            //     os << digit.words[0];   //outputs digit (stored in words[0] since value<10)
+            //     result /= ten;  //divides result by 10 (effectively right shifting in base 10)
+            // }while(!result.is_zero());
 
-            return os;
-            /* Outputs representation in base 64
-            for(int i=0; i<bigint.max_words; i++){
-                os << bigint.words[i] << "×2^" << (i>>6);
-                if(i!= bigint.max_words-1){
+            // return os;
+
+            // Outputs representation in base 64
+            for(int i=0; i<bigint.words.size(); i++){
+                os << bigint.words[i] << "×2^" << (i<<6);
+                if(i!= bigint.words.size()-1){
                     os << " + ";
                 }
             }
             return os;
-            */
+            
         }
 
         //Quick function that returns if value is equal to 0
